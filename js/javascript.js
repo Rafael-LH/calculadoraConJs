deb3135312c3c3797eedf9978e4239a81e08890e
Rafael <raciel_180@hotmail.com>
2018-03-06 14:47:37 -0600

    calculadora terminada solo falta darle un poco de estilos con css3

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,34 @@
 
         var aux = '', numeros = 0, resultado = 0;
-        var suma = false, resta = false, multiplicacion = false, divicion = false;
+        var suma = false, resta = false, multiplicacion = false, divicion = false, puntoDes = true;
 
         var borrarTodo = document.getElementById('borrarTodo');
         var cajadeTexto = document.getElementById('cajadetexto');
+
+        document.getElementById('cajadetexto').value = '0';
 
         function operacion(num){
           document.getElementById('cajadetexto').value=aux+num;
           aux = document.getElementById('cajadetexto').value;
         }
         function borrar(){
-              document.getElementById('cajadetexto').value='';
-              aux = document.getElementById('cajadetexto').value='';
+              document.getElementById('cajadetexto').value='0';
+              aux = '';
 
         }borrarTodo.addEventListener("click",borrar);
 
+        function dell(){
+                var cad =  document.getElementById('cajadetexto').value;
+                var res = cad.substring(0,cad.length-1);
+                if(res.length > 0){
+                    aux = document.getElementById('cajadetexto').value = res;
+              }else {
+                    document.getElementById('cajadetexto').value = '0';
+                    aux = '';
+              }
+
+        }
+        /*valida que no puedan ingresar texto*/
         function text(){
               cajadeTexto = document.getElementById('cajadetexto').value;
               var validaText = /^([a-zA-Z]{0,})$/;
@@ -23,50 +37,58 @@
               }
           }cajadeTexto.addEventListener('keypress',text);
 
-        /*operador*/
+        function decimal(){
+              if(puntoDes){
+                  document.getElementById('cajadetexto').value=aux+'.';
+                  aux = document.getElementById('cajadetexto').value;
+                  puntoDes = false;
+              }
+        }
+        /*operadores*/
         function Sumar(){
-          numeros =  parseInt(document.getElementById('cajadetexto').value);
+          numeros =  parseFloat(document.getElementById('cajadetexto').value);
           document.getElementById('cajadetexto').value='+';
           aux = '+';
           suma = true;
         }
         function Restar(){
-          numeros = parseInt(document.getElementById('cajadetexto').value);
+          numeros = parseFloat(document.getElementById('cajadetexto').value);
           document.getElementById('cajadetexto').value = '-';
           aux = '-';
           resta = true;
         }
         function Multiplicacion(){
-          numeros = parseInt(document.getElementById('cajadetexto').value);
+          numeros = parseFloat(document.getElementById('cajadetexto').value);
           document.getElementById('cajadetexto').value = '*';
           aux = '*';
           multiplicacion = true;
         }
         function Divicion(){
-          numeros = parseInt(document.getElementById('cajadetexto').value);
+          numeros = parseFloat(document.getElementById('cajadetexto').value);
           document.getElementById('cajadetexto').value='/';
           aux = '/';
           divicion = true;
         }
         function igual(){
               if(suma){
-                  document.getElementById('cajadetexto').value = numeros + parseInt(aux);
+                  document.getElementById('cajadetexto').value = numeros + parseFloat(aux);
                   suma = false;
               }else if(resta){
                   aux = aux.replace('-','');
-                  document.getElementById('cajadetexto').value = numeros - parseInt(aux);
+                  document.getElementById('cajadetexto').value = numeros - parseFloat(aux);
                   resta = false;
               }else if(multiplicacion){
                   aux = aux.replace('*','');
-                  document.getElementById('cajadetexto').value = numeros * parseInt(aux);
+                  document.getElementById('cajadetexto').value = numeros * parseFloat(aux);
                   multiplicacion = false;
               }
               else if (divicion) {
                   aux = aux.replace('/','');
-                  document.getElementById('cajadetexto').value = numeros / parseInt(aux);
+                  document.getElementById('cajadetexto').value = numeros / parseFloat(aux);
                   divicion = false;
               }
               aux = '';
+              puntoDes = true;
         }
 
 
@@ -76,4 +98,4 @@
 
 
 
-/**/
+/*end*/
