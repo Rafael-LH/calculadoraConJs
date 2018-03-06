f24a93c841a898f6e406ba0a3f0552d631813eba
Rafael <raciel_180@hotmail.com>
2018-03-06 13:04:11 -0600

    operacones terminadas de suma, resta, multiplicacion y divicion, casi terminada la calculadora basica

--- js/javascript.js
+++ js/javascript.js
@@ -12,8 +12,8 @@
         function borrar(){
               document.getElementById('cajadetexto').value='';
               aux = document.getElementById('cajadetexto').value='';
-        }
-          borrarTodo.addEventListener("click",borrar);
+
+        }borrarTodo.addEventListener("click",borrar);
 
         function text(){
               cajadeTexto = document.getElementById('cajadetexto').value;
@@ -21,5 +21,59 @@
               if(validaText.test(cajadeTexto) ){
                     document.getElementById('cajadetexto').value = '';
               }
+          }cajadeTexto.addEventListener('keypress',text);
 
-        }cajadeTexto.addEventListener('keypress',text);
+        /*operador*/
+        function Sumar(){
+          numeros =  parseInt(document.getElementById('cajadetexto').value);
+          document.getElementById('cajadetexto').value='+';
+          aux = '+';
+          suma = true;
+        }
+        function Restar(){
+          numeros = parseInt(document.getElementById('cajadetexto').value);
+          document.getElementById('cajadetexto').value = '-';
+          aux = '-';
+          resta = true;
+        }
+        function Multiplicacion(){
+          numeros = parseInt(document.getElementById('cajadetexto').value);
+          document.getElementById('cajadetexto').value = '*';
+          aux = '*';
+          multiplicacion = true;
+        }
+        function Divicion(){
+          numeros = parseInt(document.getElementById('cajadetexto').value);
+          document.getElementById('cajadetexto').value='/';
+          aux = '/';
+          divicion = true;
+        }
+        function igual(){
+              if(suma){
+                  document.getElementById('cajadetexto').value = numeros + parseInt(aux);
+                  suma = false;
+              }else if(resta){
+                  aux = aux.replace('-','');
+                  document.getElementById('cajadetexto').value = numeros - parseInt(aux);
+                  resta = false;
+              }else if(multiplicacion){
+                  aux = aux.replace('*','');
+                  document.getElementById('cajadetexto').value = numeros * parseInt(aux);
+                  multiplicacion = false;
+              }
+              else if (divicion) {
+                  aux = aux.replace('/','');
+                  document.getElementById('cajadetexto').value = numeros / parseInt(aux);
+                  divicion = false;
+              }
+              aux = '';
+        }
+
+
+
+
+
+
+
+
+/**/
